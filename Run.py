6df21a3c754542ac45291ad5350c44bc4a4ec7b7
Matthew Barthet <matthew.barthet@um.edu.mt>
2024-03-19 14:08:07 +0100

    ACII Experiments Results so far

--- Run.py
+++ Run.py
@@ -2,7 +2,7 @@ import subprocess
 
 for cluster in [0, 1, 2, 3]:
     for run in [1, 2, 3]:
-        for target_name, target_signal in [("Maximize", "np.ones")]:
+        for target_name, target_signal in [("imitate", "imitate")]:
             preference_trask = True
             script_path = './PCGEnvironment_Go-Explore.py'
             command = f'cd Desktop/Affect-Driven-RL && conda activate unity_gym && python {script_path} {cluster} {run} {target_name} {target_signal} {preference_trask} False'
